5ceb2085627481ee5447975e4db8eb1bf39cf0bb
SeyoungLee <sylee004@gmail.com>
2019-12-08 11:57:40 +0900

    im branch

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,7 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
-  console.log("heyyyy")
-  console.log("why are you so serious??")
+  console.log("i just has one!!!")
 }
 bootstrap();
