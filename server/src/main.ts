35c7712733090a623a0fbc7b48be39f28f6a405e
SeyoungLee <sylee004@gmail.com>
2019-12-08 12:06:04 +0900

    just you commit

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
-  console.log("heyyyy")
+  console.log("heyyyy hoyw are you?")
   console.log("why are you so serious??")
 }
 bootstrap();
