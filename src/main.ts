abfb1c43d32d8557be43d0c4639ddce638232035
SeyoungLee <sylee004@gmail.com>
2019-12-08 11:53:48 +0900

    hay

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
-  console.log("i just has one")
+  console.log("i just has one!!!")
 }
 bootstrap();
